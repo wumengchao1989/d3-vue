e03d7f7e82c373988cee244949b01e498781fde6
wumc <wumc@xingxy.cn>
2018-02-21 17:44:00 +0800

    add pie data

--- src/components/Shape/Pie.vue
+++ src/components/Shape/Pie.vue
@@ -17,42 +17,49 @@
             createArc(){
                 const svg = d3.select('#content').append('svg').attr('width', 1200).attr("height", 780);
                 const arcs = d3.pie()(this.$store.state.pie_M.pieData)
-                const arc = d3.arc();
+                const arc = d3.arc().cornerRadius(5);
                 arcs.map((item, index) => {
                     let that = this;
-                    let arcLine = arc({
-                        innerRadius: 40,
-                        outerRadius: 200,
-                        startAngle: item.startAngle,
-                        endAngle: item.endAngle,
-                        cornerRadius: Math.PI / 3,
-                        padAngle: 0.04
-                    });
+                    let arcLine = d3.arc()
+                        .innerRadius(40)
+                        .outerRadius(200)
+                        .startAngle(item.startAngle)
+                        .endAngle(item.endAngle)
+                        .padAngle(0.04)
+                        .cornerRadius(5);
+                    let centroid=arcLine.centroid();
                     svg.append("path")
                         .attr("transform", "translate(400,300)")
                         .attr("d", arcLine)
-                        .attr("stroke", "red")
+                        .attr("stroke", "black")
                         .attr("fill", this.$store.state.pie_M.pieColor[index])
                         .on('mouseover', function () {
-                            that.zoom(this, item,"in")
+                            that.zoom(this, item, "in")
                         })
-                        .on('mouseout',function(){
-                            that.zoom(this,item,"out")
+                        .on('mouseout', function () {
+                            that.zoom(this, item, "out")
                         });
-                    svg.selectAll("text").enter()
+                    svg.append('text').attr("x", centroid[0] + 392)
+                        .attr("y", centroid[1] + 308)
+                        .text(item.value)
                 })
             },
             zoom(){
-                const arc = d3.arc();
-                let arcZoom = arc({
-                    innerRadius: 40,
-                    outerRadius: arguments[2]=="in"?300:200,
-                    startAngle: arguments[1].startAngle,
-                    endAngle: arguments[1].endAngle,
-                    cornerRadius: Math.PI / 3,
-                    padAngle: 0.04
-                });
-                d3.select(arguments[0]).attr("d", arcZoom)
+                let el = arguments[0];
+                let item = arguments[1];
+                let zoomFlag = arguments[2];
+                let arcZoom = d3.arc()
+                    .innerRadius(40)
+                    .outerRadius(zoomFlag == "in" ? 300 : 200,)
+                    .startAngle(item.startAngle)
+                    .endAngle(item.endAngle)
+                    .padAngle(0.04)
+                    .cornerRadius(5);
+                d3.select(el)
+                    .transition()
+                    .ease(d3.easeBounceOut)
+                    .duration(1500)
+                    .attr("d", arcZoom);
             },
 
         }
